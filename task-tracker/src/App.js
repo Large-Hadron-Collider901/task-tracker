3435a92724a76c50e7df0c2039c60ee6a925f2d5
Skye M <skyemc608@gmail.com>
2021-09-18 23:52:42 -0500

    added styling on the button

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -5,6 +5,7 @@ import AddTask from './components/AddTask'
 
 
 const App = () => {
+    const [showAddTask, setShowAddTask] =useState(false)
     const [tasks, setTasks] = useState([
         {
         id: 1,
@@ -58,8 +59,8 @@ setTasks(
 
   return (
     <div className="container">
-     <Header />
-     <AddTask onAdd={addTask} />
+     <Header onAdd={() => setShowAddTask (!showAddTask)} showAdd={showAddTask}/>
+     {showAddTask && <AddTask onAdd={addTask} />}
     {tasks.length > 0 ? (
     <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
     ) : (
